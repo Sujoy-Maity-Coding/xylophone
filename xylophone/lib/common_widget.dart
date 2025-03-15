12609f13a5b19b07422afb90e38b59472cab9c91
Sujoy-Maity-Coding <sujoymaity526@gmail.com>
2025-03-15 20:49:18 +0530

    update changes

---
+++ xylophone/lib/common_widget.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+
+class CommonWidget extends StatelessWidget {
+  final Color color;
+  final int audioNumber;
+
+  const CommonWidget({
+    super.key,
+    required this.color,
+    required this.audioNumber,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      child: Container(
+        height: MediaQuery.of(context).size.height / 7,
+        width: MediaQuery.of(context).size.width,
+        color: color,
+      ),
+      onTap: () {
+        final player = AudioPlayer();
+        player.play(AssetSource('assets_note${audioNumber}.wav'));
+      },
+    );
+  }
+}
